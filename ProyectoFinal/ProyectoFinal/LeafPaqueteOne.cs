75723d4e5335792cfecc10c6453f97b4d0d9aa76
Alejandro Uriarte <alejandro.uriartepatsi@gmail.com>
2024-05-06 22:55:03 -0400

    Implementados los patrones composite y decorator

---
+++ ProyectoFinal/ProyectoFinal/LeafPaqueteOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal
+{
+    internal class LeafPaqueteOne : ComponentPaqueteBasico
+    {
+        public string Habitacion { get; set; }
+
+        public LeafPaqueteOne(string nombre) : base(nombre)
+        {
+            Habitacion = "1 habitación";
+        }
+
+        public override void DetallesPaquete() 
+        {
+            Console.WriteLine($"{base.Name}: {Habitacion}");
+        }
+    }
+}
