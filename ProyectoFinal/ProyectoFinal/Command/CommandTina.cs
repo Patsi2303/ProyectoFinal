1667e180af2fb3ea26a071ddde0e8eccfbe70dca
Eitan <eitandelajaille1@upb.edu>
2024-05-07 14:18:13 -0400

    Creacion del patron command

---
+++ ProyectoFinal/ProyectoFinal/Command/CommandTina.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Command
+{
+    internal class CommandTina : CACommand
+    {
+        public CommandTina(ReceiverHabitacion habitacion) : base(habitacion) { }
+        public override void ejecutar()
+        {
+            Console.WriteLine("Ingrese la temperatura del agua para llenar la tina:");
+            string temperatura = Console.ReadLine();
+            Console.WriteLine("Ingrese el horario en el que desea llenar la tina");
+            string horario = Console.ReadLine();
+            habitacion.seleccionarTemperaturaTina(temperatura, horario);
+        }
+    }
+}
---
+++ ProyectoFinal/ProyectoFinal/Command/ReceiverHabitacion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Command
+{
+    internal class ReceiverHabitacion
+    {
+        public void seleccionarArtista(string artista, string horario)
+        {
+            Console.WriteLine($"Reproduciendo {artista} en horario: {horario}");
+        }
+        public void seleccionarVolumen(string volumen, string horario) 
+        {
+            Console.WriteLine($"Ajustando volumen a: {volumen} en horario: {horario}");
+        }
+        public void seleccionarTemperaturaTina(string temperatura, string horario)
+        {
+            Console.WriteLine($"Llenando tina a temperatura: {temperatura}°C en horario: {horario}");
+        }
+        public void seleccionarIntensidadLuz(string intensidad, string horario)
+        {
+            if (intensidad == "1")
+                Console.WriteLine($"Seleccionando intensidad LEVE de luz en horario: {horario}");
+            if (intensidad == "2")
+                Console.WriteLine($"Seleccionando intensidad MEDIA de luz en horario: {horario}");
+            else
+                Console.WriteLine($"Seleccionando intensidad ALTA de luz en horario: {horario}");
+            
+        }
+        public void seleccionarCortinas(string abiertocerrado, string horario)
+        {
+            if (abiertocerrado == "1")
+                Console.WriteLine($"Abriendo cortinas en horario: {horario}");
+            else
+                Console.WriteLine($"Cerrando cortinas en horario: {horario}");
+        }
+
+    }
+}
