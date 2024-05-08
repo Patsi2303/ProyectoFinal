64386cbbdd6e9978a8caea9537aa5efd111df667
Alejandro Uriarte <alejandro.uriartepatsi@gmail.com>
2024-05-08 11:50:40 -0400

    Merge con Command y cambios en el program

--- ProyectoFinal/ProyectoFinal/Program.cs
+++ ProyectoFinal/ProyectoFinal/Program.cs
@@ -9,27 +9,6 @@ namespace ProyectoFinal
     {
         static void Main(string[] args)
         {
-            /*Director miDirector = new Director();
-            BuilderInteligente builderInteligente = new BuilderInteligente();
-
-            MediatorRecepcion miRecepcion = new MediatorRecepcion();
-
-            ComponentPaquete miPaquete1;
-            ComponentPaquete miPaquete2;
-            ComponentPaquete miPaquete3;
-
-            miPaquete1 = miRecepcion.generarPaqueteOne(miDirector.seleccionarHabitacion(builderInteligente, 1));
-            miPaquete2 = miRecepcion.generarPaqueteMedium(miDirector.seleccionarHabitacion(builderInteligente, 2));
-            miPaquete3 = miRecepcion.generarPaqueteBig(miDirector.seleccionarHabitacion(builderInteligente, 3));
-
-            miPaquete1.MostrarPrecio();
-            miPaquete2.MostrarPrecio();
-            miPaquete3.MostrarPrecio();
-
-            miPaquete1 = new DecoratorInternet(miPaquete1);
-            miPaquete1.MostrarPrecio();
-            miPaquete1.DetallesPaquete();*/
-
             Cliente miCliente = new Cliente();
 
             int continuar = 1;
